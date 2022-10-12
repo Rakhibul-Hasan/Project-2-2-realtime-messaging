640cdfc495bb63bd0a6987bcedb7f0e6f957dbaa
Msohelrana <99506216+Msohelrana@users.noreply.github.com>
2022-10-12 12:14:05 +0600

    This is the index page .Here we use HTML & CSS

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>HeyBuddy</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="index.css">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+</head>
+<body>
+    <nav>
+        <div class="left">
+          <a href="#">  <h3>HeyBuddy</h3></a>
+        </div>
+        <div class="right">
+           <a href="login.php">Sign In</a>
+           <button class="btn"><a href="signup.php">Start for free</a></button>
+        </div>
+
+    </nav>
+    <main>
+        <div class="left">
+         <h3 id="heybuddi">HeyBuddy</h3>
+         <h1>A Real-time Messageing Platform</h1>
+         <p>Connect with your friends, foe & relatives through our chatting application.</p>
+         <button class="btn"><a href="signup.php">Sign Up Now</a></button>
+        </div>
+        <div class="right">
+            <img src="2.png" alt="">
+        </div>
+    </main>
+</body>
+</html>
+
