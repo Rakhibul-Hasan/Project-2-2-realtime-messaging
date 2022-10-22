4c1735e066f6ea32fc6dcfed2d3fbfbfeeec9183
Msohelrana <99506216+Msohelrana@users.noreply.github.com>
2022-10-22 22:04:04 +0600

    contact page

---
+++ contact.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+    <link rel="stylesheet" href="contact.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+    <h1 class="heading">Contact Us</h1>
+     <div class="container">
+
+        <div class="form">
+            <h2></h2>
+            <div class="col2">
+                <label for="name">FULL NAME  <br>
+                    <input type="text" placeholder="Name">
+                </label>
+               
+                <label for="email">EMAIL <br> <input type="email" placeholder="Email"></label>
+                
+            </div>
+            <label for="subject">SUBJECT  <br>
+                <input type="text" placeholder="Subject">
+            </label>
+           
+            <label for="message">MESSAGE  <br>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </label>
+            <a href="mailto:heybuddy494949@gmail.com"><input class="btn" type="submit" name="" id=""b value="Send Message"></a>
+            
+        </div>
+        <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d14594.197245676223!2d90.27139549761831!3d23.870132554972066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sJahangirnaga%20University!5e0!3m2!1sen!2sbd!4v1666428100696!5m2!1sen!2sbd"  style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+        </div>
+     </div>
+     <div class="address">
+        <div class="icon">
+            <i class="fa-solid fa-location-dot"></i>
+              <p>Address: <span style="color:#110000;">Jahangirnagar University, <br>Savar,Dhaka,Bangladesh</span></p>
+        </div>
+        <div class="icon">
+            <i class="fa-solid fa-phone"></i>
+            <p>Phone: <span style="color:blue">+8801822293027</span></p>
+        </div>
+        <div class="icon">
+            <i class="fa-solid fa-paper-plane"></i>
+            <p>Email: <a href="heybuddy494949@gmail.com.com"><span style="color: blue;">heybuddy494949@gmail.com</span></a></p>
+        </div>
+        <div class="icon">
+            <i class="fa-solid fa-earth-americas"></i>
+            <p>Website: <a href="#">heybuddy.com</a></p>
+        </div>
+     </div>
+</body>
+</html>
